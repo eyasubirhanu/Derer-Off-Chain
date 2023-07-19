22667e976c85d7e76746cb3496ce86d4828a7635
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-07-19 14:46:30 +0300

    edit the mint and burn input

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -38,17 +38,15 @@ export default function Mint() {
       if (utility.isError(e)) showToaster("Could not burn NFT", e.message)
       else if (typeof e === "string") showToaster("Could not burn NFT", e)
     }
-  }, [lucid, account?.address, showToaster, name])
+  }, [lucid, account?.address, showToaster, burnname])
 
   const canMint = useMemo(() => lucid && account?.address && name, [lucid, account?.address, name])
 
 
-
+  const canBurn = useMemo(() => lucid && account?.address && name, [lucid, account?.address, burnname])
 
 return (
   <div className="text-center text-gray-900 dark:text-gray-100 ">
-    <div className="absolute right-40 rounded-xl bg-sky-800 py-2  hover:opacity-80 duration-200 disabled:opacity-30 w-40 text-white">
-    </div>
     <h1
       style={inter.style}
       className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl py-2"
@@ -110,7 +108,7 @@ return (
               </div>
         </div>
         <button
-          disabled={!canMint}
+          disabled={!canBurn}
           className="border hover:bg-blue-400 text-white my-4 w-64 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-red-300 disabled:bg-red-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
           onClick={() => {
             hideToaster();
@@ -120,6 +118,38 @@ return (
           burn
         </button>
       </div>
+        <div className="w-[500px] max-w-[90%] h-[550px] mt-10 shadow-sm bg-slate-800 rounded-xl flex items-center flex-col p-8 ">
+        <div className="rounded-xl text-xl font-bold mb-6 text-white">
+          List NFT on MarketPlace.
+        </div>
+
+      <div className="text-left my-8">
+              <div className="my-4">
+                <label className="flex flex-col w-100">
+                  <span className="text-sm lowercase mb-1">NFT Name</span>
+
+                  <input
+                    className="rounded py-1 px-2 text-gray-800 border"
+                    name="message"
+                    placeholder="My NFT name"
+                    value={name || ""}
+                    onChange={(e) => setName(e.target.value?.toString())}
+                  />
+                </label>
+              </div>
+        </div>
+        <button
+          disabled={!canMint}
+          className="border hover:bg-blue-800 text-white my-4 w-64 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
+          onClick={() => {
+            hideToaster();
+            // mintNFT()
+          }}
+        >
+          List
+        </button>
+      </div>
+  
     </div>
     </div>
 )
