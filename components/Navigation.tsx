ffa3139f63667beb045319b0dea5f4e3c3d404b7
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-08-11 15:09:49 +0300

    navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -15,7 +15,7 @@ export const Navigation = () => {
 
   const isHome = useMemo(() => asPath === "/", [asPath])
   const isTransact = useMemo(() => asPath === "/transact", [asPath])
-  // const isSign = useMemo(() => asPath === "/sign", [asPath])
+  const isSign = useMemo(() => asPath === "/sign", [asPath])
   const isMint = useMemo(() => asPath === "/mint", [asPath])
 
   return (
@@ -30,7 +30,7 @@ export const Navigation = () => {
         </Link>
 
         <Link href="/sign">
-          {/* <button className={twMerge(className, isSign && activeClassName)}>Sign</button> */}
+          <button className={twMerge(className, isSign && activeClassName)}>Sign</button>
         </Link>
 
         <Link href="/mint">
