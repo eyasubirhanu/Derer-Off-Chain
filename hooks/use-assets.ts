4e8bcf7c0f4b95c882a90a8f9d2de876260fad6b
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-05 12:12:29 +0300

    edit use-assets hook

--- hooks/use-assets.ts
+++ hooks/use-assets.ts
@@ -1,74 +1,103 @@
-import { Lucid,fromUnit} from "lucid-cardano"
-import { Responses } from '@blockfrost/blockfrost-js';
-import { sortBy } from "lodash"
-import { useCallback, useEffect, useState } from "react"
+import { Lucid, fromUnit } from "lucid-cardano";
+import { sortBy } from "lodash";
+import { useCallback, useEffect, useState } from "react";
 
-
-type Value = ReturnType<typeof fromUnit>
+type Value = ReturnType<typeof fromUnit>;
 
 interface ValueWithName extends Omit<Value, "name"> {
-  value: number
-  name: string
+  value: number;
+  name: string;
 }
 
- const listAssets = (lucid?:Lucid) => {
+const listAssets = (lucid?: Lucid) => {
+  const [assets, setAssets] = useState<ValueWithName[]>([]);
+  const [lovelace, setLovelace] = useState(0);
 
-    const [assets, setAssets] = useState<Responses["asset"][]>([])
-    const [lovelace, setLovelace] = useState(0)
+  const fetchAssets = useCallback(async () => {
+    if (!lucid?.wallet) return;
 
-    const fetchAssets = useCallback(async () => {
-        if (!lucid?.wallet) return
-        const utxos = await lucid.wallet.getUtxos()
-        const allUtxos = utxos
-        .map((u) => Object.keys(u.assets).map((key) => ({ key, value: u.assets[key] })))
-        .reduce((acc, curr) => [...acc, ...curr], [])
-        .map((a) => ({
+    const utxos = await lucid.wallet.getUtxos();
+
+    const allUtxos = utxos
+      .map((u) =>
+        Object.keys(u.assets).map((key) => ({ key, value: u.assets[key] }))
+      )
+      .reduce((acc, curr) => [...acc, ...curr], [])
+      .map((a) => ({
         ...fromUnit(a.key),
         value: Number(a.value),
-        }))
-        
+      }));
+
     const lovelaces = allUtxos
       .filter((u) => u.policyId === "lovelace")
-      .reduce((acc, curr) => acc + curr.value, 0)
-  
+      .reduce((acc, curr) => acc + curr.value, 0);
+
     const utxoAssets = allUtxos
       .filter((u) => u.policyId !== "lovelace")
       .filter((v: Value): v is ValueWithName => v.name !== null)
       .map((a) => ({
         ...a,
         fullyQualifiedAssetName: `${a.policyId}${a.name}`,
-      }))
-  
-      const assetsWithMetadata: Responses["asset"][] = await Promise.all(
-        utxoAssets.map((a) =>
-          fetch(`/api/blockfrost/${"Preprod"}/assets/${a.fullyQualifiedAssetName}`).then((r) =>
-            r.json()
-          )
-        )
-      )
-  
-    const sortedAssets = sortBy(
-      assetsWithMetadata,
-        (a) => (Number(a.quantity) === 1 ? 1 : -1),
+      }));
+
+    // Fetch asset metadata for each asset in utxoAssets
+    const assetMetadataPromises = utxoAssets.map((a) =>
+      fetch(
+        `/api/blockfrost/${"Preprod"}/assets/${a.fullyQualifiedAssetName}`
+      ).then((r) => r.json())
+    );
+
+    try {
+      const assetsWithMetadata = await Promise.all(assetMetadataPromises);
+
+      // Ensure that assetsWithMetadata is correctly structured as an array of objects
+      const sortedAssets = sortBy(
+        assetsWithMetadata,
+        (a: { quantity: any }) => (Number(a.quantity) === 1 ? 1 : -1),
         "policy_id",
         "metadata.name",
         "onchain_metadata.name"
-      )
-    // setAssets(sortedAssets)
-    setLovelace(lovelaces)
-    setAssets(sortedAssets)
-    // console.log(assetsWithMetadata,"mintadd");
-    // console.log(sortedAssets, "eyasumintadd");
-    },[lucid?.wallet])
+      );
 
-    useEffect(() => {
-        fetchAssets()
-      }, [fetchAssets])
+      setLovelace(lovelaces);
 
-    return {
-        lovelace,
-        assets,
-    }  
-}
+      // Update assets state with the correctly structured data
+      setAssets(sortedAssets);
+    } catch (error) {
+      // Handle errors if any occur during API requests
+      console.error("Error fetching asset metadata:", error);
+    }
+  }, [lucid?.wallet]);
 
-export { listAssets }
+  useEffect(() => {
+    fetchAssets();
+  }, [assets, lovelace]);
+
+  return {
+    lovelace,
+    assets,
+  };
+};
+
+export { listAssets };
+
+// const Blockfrost = require("@blockfrost/blockfrost-js");
+// // import { BlockFrostIPFS } from '@blockfrost/blockfrost-js'; // using import syntax
+
+// const IPFS = new Blockfrost.BlockFrostIPFS({
+//   projectId: "ipfsuwPRuQaISYnggAmqZV5i1zKH8otvB9ZR", // see: https://blockfrost.io
+// });
+
+// async function runExample() {
+//   try {
+//     const added = await IPFS.add(`${__dirname}/img.svg`);
+//     console.log("added", added);
+
+//     const pinned = await IPFS.pin(added.ipfs_hash);
+//     console.log("pinned", pinned);
+//   } catch (err) {
+//     console.log("error", err);
+//   }
+// }
+
+// runExample();
