379853f4705830949a7235fce48285bf417ed676
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-05 15:27:43 +0300

    edit sign page

--- pages/sign.tsx
+++ pages/sign.tsx
@@ -1,73 +1,3 @@
-// import { listAssets } from "hooks/use-assets"
-// import { useCardano, utility } from "use-cardano"
-// import { isNil } from "lodash"
-// import { Inter } from "@next/font/google"
-// import styles from "../styles/index.module.css"
-// import { lovelaceToAda } from "lib/lovelace-to-ada"
-
-// const inter = Inter({ subsets: ["latin"] })
-//   const Index = () => {
-//     const { lucid, account, showToaster, hideToaster } = useCardano()
-//     const { lovelace, assets } = listAssets(lucid)
-//     return (
-//       <div className={styles.container}>
-//         <h1 className={styles.title}>Listing Assets Example</h1>
-
-//         {lovelace > 0 && <div>ADA: {lovelaceToAda(lovelace)}</div>}
-
-//         <div>
-//           <ul className={styles.list}>
-//             {assets.map((a) => {
-//               const name = a.metadata?.name || a.onchain_metadata?.name || ""
-
-//               return (
-//                 <li key={a.asset}>
-//                   <div>
-//                     {name}
-
-//                     {Number(a.quantity) > 1 &&
-//                       ` (${lovelaceToAda(
-//                         Number(a.quantity) / Math.pow(10, a.metadata?.decimals || 0)
-//                       )})`}
-//                   </div>
-
-//                   {!isNil(a.metadata?.logo) && (
-//                     <div>
-//                       {/* eslint-disable-next-line @next/next/no-img-element */}
-//                       <img
-//                         width="64"
-//                         height="auto"
-//                         alt={`${name} logo`}
-//                         src={`data:image/png;base64,${a.metadata?.logo}`}
-//                       />
-//                     </div>
-//                   )}
-
-//                   {!isNil(a.onchain_metadata?.image) && (
-//                     <div>
-//                       {/* eslint-disable-next-line @next/next/no-img-element */}
-//                       <img
-//                         width="64"
-//                         height="auto"
-//                         alt={`${name} NFT`}
-//                         src={(a.onchain_metadata?.image as string).replace(
-//                           "ipfs://",
-//                           "https://ipfs.blockfrost.dev/ipfs/"
-//                         )}
-//                       />
-//                     </div>
-//                   )}
-//                 </li>
-//               )
-//             })}
-//           </ul>
-//         </div>
-//       </div>
-//     )
-//   }
-
-//   export default Index
-
 import React, { useState } from "react";
 import { useCardano } from "use-cardano";
 import { Inter } from "@next/font/google";
@@ -138,6 +68,7 @@ const Index = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {assets.map((a, index) => {
             const name = a.metadata?.name || a.onchain_metadata?.name || "";
+            const tokenname = a.name;
             const description = a.metadata?.description || "";
             const imageSrc =
               a.onchain_metadata?.image?.replace(
@@ -161,6 +92,7 @@ const Index = () => {
                   />
                 )}
                 <h3 className="text-xl font-bold mb-2">{name}</h3>
+                <h3 className="text-xl font-bold mb-2">{tokenname}</h3>
                 <p className="text-gray-600">{description}</p>
                 <div className="flex justify-between items-center mt-6">
                   <input
