285e0575a57b7c4aaba5dc1660c614d55f84ae06
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-26 16:27:57 +0300

    update listAssets function added script address

--- hooks/use-assets.ts
+++ hooks/use-assets.ts
@@ -1,10 +1,13 @@
-import { Lucid, fromUnit } from "lucid-cardano";
+import { Lucid, fromUnit, Address } from "lucid-cardano";
 import { sortBy } from "lodash";
 import { useCallback, useEffect, useState } from "react";
+import { assets } from "@blockfrost/blockfrost-js/lib/endpoints/api/assets";
+import * as pool from "lib/poolnfts";
 
 type Value = ReturnType<typeof fromUnit>;
 
 interface ValueWithName extends Omit<Value, "name"> {
+  [x: string]: any;
   value: number;
   name: string;
 }
@@ -16,9 +19,26 @@ const listAssets = (lucid?: Lucid) => {
   const fetchAssets = useCallback(async () => {
     if (!lucid?.wallet) return;
 
+    // Utxo from user wallet
     const utxos = await lucid.wallet.getUtxos();
 
-    const allUtxos = utxos
+    // Utxo from address
+    const poolAddress: Address = lucid.utils.validatorToAddress(
+      pool.poolScript
+    );
+    const utx = await lucid.utxosAt(poolAddress);
+
+    // output example of allUtxos
+    // [
+    // {
+    //   policyId: "policy1",
+    //   assetName: "asset1",
+    //   name: "Asset One",
+    //   label: null,
+    //   value: 10,
+    // },
+    // ]
+    const allUtxos = utx
       .map((u) =>
         Object.keys(u.assets).map((key) => ({ key, value: u.assets[key] }))
       )
@@ -28,10 +48,12 @@ const listAssets = (lucid?: Lucid) => {
         value: Number(a.value),
       }));
 
+    // Calculate the total quantity of "lovelace" assets from a list of UTXOs.
     const lovelaces = allUtxos
       .filter((u) => u.policyId === "lovelace")
       .reduce((acc, curr) => acc + curr.value, 0);
 
+    // Filter and transform UTXOs to select assets other than "lovelace" and add a fully qualified asset name.
     const utxoAssets = allUtxos
       .filter((u) => u.policyId !== "lovelace")
       .filter((v: Value): v is ValueWithName => v.name !== null)
@@ -51,10 +73,12 @@ const listAssets = (lucid?: Lucid) => {
       const assetsWithMetadata = await Promise.all(assetMetadataPromises);
 
       // Ensure that assetsWithMetadata is correctly structured as an array of objects
+      // Sort assets with metadata based on quantity after that by, policy ID, name, metadata name, and onchain metadata name.
       const sortedAssets = sortBy(
         assetsWithMetadata,
         (a: { quantity: any }) => (Number(a.quantity) === 1 ? 1 : -1),
         "policy_id",
+        "name",
         "metadata.name",
         "onchain_metadata.name"
       );
@@ -71,7 +95,7 @@ const listAssets = (lucid?: Lucid) => {
 
   useEffect(() => {
     fetchAssets();
-  }, [assets, lovelace]);
+  }, [fetchAssets]);
 
   return {
     lovelace,
