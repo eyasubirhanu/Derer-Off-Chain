30c3bbd29b2349bd5e3d92ab16d3c0f946e68198
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-26 16:31:20 +0300

    added fetchRate and edit buyNFT function

--- pages/transact.tsx
+++ pages/transact.tsx
@@ -1,43 +1,94 @@
 import { listAssets } from "hooks/use-assets";
 import { useCardano, utility } from "use-cardano";
-// import { isNil } from "lodash";
 import { Inter } from "@next/font/google";
 import styles from "../styles/index.module.css";
-import { lovelaceToAda } from "lib/lovelace-to-ada";
-import { useCallback, useMemo, useState, useEffect } from "react";
 import * as pool from "lib/poolnfts";
+import { useCallback, useMemo, useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Index = () => {
-  const { account, showToaster, hideToaster } = useCardano();
-  const [name, setName] = useState("");
-  const { lucid } = useCardano();
-  const { lovelace, assets } = listAssets(lucid);
+  const { lucid, showToaster } = useCardano();
+  const { assets } = listAssets(lucid);
+  const [rate, setRate] = useState(0n); // Initialize rate as 0n (bigint)
+  const [isTransactionPending, setTransactionPending] = useState(false); // Flag to prevent multiple transactions
 
-  const MPbuyNFT = useCallback(
-    async (tokenname: string, tokenpolicyid: string) => {
-      try {
-        if (!lucid || !name) return;
-        // pool.displayNft(lucid, name).then((r) => setDisplay(r));
-        // const [named, setName] = useState("")
+  const fetchRate = async (asset) => {
+    try {
+      if (!lucid || !asset) return;
 
-        const buyTx = await pool.buyNFT({
-          lucid,
-          address: "",
-          name: tokenname,
-          policyid: tokenpolicyid,
-        });
+      // Fetch the rate
+      const {
+        metadata,
+        asset: { policyId },
+      } = asset;
 
-        // const tokenimage = await nftTx.assetMetadata.image
-        showToaster("Buy NFT", `Transaction: ${buyTx}`);
-      } catch (e) {
-        if (utility.isError(e)) showToaster("Could not mint NFT", e.message);
-        else if (typeof e === "string") showToaster("Could not mint NFT", e);
-      }
-    },
-    [lucid, showToaster, name]
-  );
+      const assetName = metadata?.name || asset.onchain_metadata?.name || "";
+      console.log(asset, "asset");
+
+      const policyIdd = asset.policy_id;
+      const tokenname = asset.asset_name;
+      console.log(policyIdd, "policyid");
+      console.log(tokenname, "tokenname");
+
+      const rateValue = await pool.getDatumValue(lucid, policyIdd, tokenname);
+      setRate(rateValue);
+    } catch (e) {
+      if (utility.isError(e)) showToaster("Could not fetch rate", e.message);
+      else if (typeof e === "string") showToaster("Could not fetch rate", e);
+    }
+  };
+
+  const buyNFT = async (asset) => {
+    try {
+      if (!lucid || !asset || isTransactionPending) return;
+
+      // Set the flag to prevent multiple transactions
+      setTransactionPending(true);
+
+      // Fetch the rate
+      await fetchRate(asset);
+
+      // Buy the NFT
+      const {
+        metadata,
+        asset: { policyId },
+      } = asset;
+
+      const assetName = metadata?.name || asset.onchain_metadata?.name || "";
+      console.log(asset, "asset");
+
+      const policyIdd = asset.policy_id;
+      const tokenname = asset.asset_name;
+      console.log(policyIdd, "policyid");
+      console.log(tokenname, "tokenname");
+
+      const buyTx = await pool.buyNFT({
+        lucid,
+        address: "", // Replace with a valid address
+        name: tokenname,
+        policyid: policyIdd,
+      });
+
+      showToaster("Buy NFT", `Transaction: ${buyTx}`);
+
+      // Reset the flag after the transaction is complete
+      setTransactionPending(false);
+    } catch (e) {
+      // Handle errors and reset the flag on error as well
+      setTransactionPending(false);
+
+      if (utility.isError(e)) showToaster("Could not buy NFT", e.message);
+      else if (typeof e === "string") showToaster("Could not buy NFT", e);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch the rate for the first asset when the component mounts
+    if (assets.length > 0) {
+      fetchRate(assets[0]);
+    }
+  }, [assets, lucid, showToaster]);
 
   return (
     <div
@@ -48,85 +99,36 @@ const Index = () => {
         padding: "2rem",
       }}
     >
-      <div
-        className="max-w-6xl mx-auto"
-        style={{
-          background: "rgba(255, 255, 255, 0.05)",
-          padding: "2rem",
-          borderRadius: "0.5rem",
-          marginBottom: "2rem",
-        }}
-      >
-        <h1
-          style={inter.style}
-          className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white"
-        >
-          Welcome to the NFT Marketplace
-        </h1>
-
-        <div className="my-4 text-center text-lg text-white">
-          Explore a collection of unique digital assets
-        </div>
-
-        {lovelace > 0 && (
-          <div className="text-2xl font-semibold my-4 text-white">
-            Total ADA: {lovelaceToAda(lovelace)}
-          </div>
-        )}
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {assets.map((a) => {
-            const name = a.metadata?.name || a.onchain_metadata?.name || "";
-            const description = a.metadata?.description || "";
-            const imageSrc =
-              a.onchain_metadata?.image?.replace(
-                "ipfs://",
-                "https://ipfs.blockfrost.dev/ipfs/"
-              ) || "";
-
-            return (
-              <div
-                key={a.asset}
-                className="nft-card bg-white shadow-lg p-5 rounded-lg"
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {assets.map((asset, index) => (
+          <div
+            key={index}
+            className="nft-card bg-white shadow-lg p-5 rounded-lg"
+          >
+            {asset?.metadata?.image && (
+              <img
+                src={`https://ipfs.blockfrost.dev/ipfs/${asset.metadata.image}`}
+                alt={`${
+                  asset.metadata?.name || asset.onchain_metadata?.name || ""
+                } NFT`}
+                className="rounded-lg mb-4"
+              />
+            )}
+            <h3 className="text-xl font-bold mb-2">
+              {asset.metadata?.name || asset.onchain_metadata?.name || ""}
+            </h3>
+            <p className="text-gray-600">{asset.metadata?.description || ""}</p>
+            <div className="flex justify-between items-center mt-6">
+              <span className="text-gray-500">Price = ${Number(rate)}</span>
+              <button
+                className={`${styles.btn} ${styles.btnPrimary}`}
+                onClick={() => buyNFT(asset)}
               >
-                {imageSrc && (
-                  <img
-                    src={imageSrc}
-                    alt={`${name} NFT`}
-                    className="rounded-lg mb-4"
-                  />
-                )}
-                <h3 className="text-xl font-bold mb-2">{name}</h3>
-                <p className="text-gray-600">{description}</p>
-                <div className="flex justify-between items-center mt-6">
-                  <span className="text-gray-500">$Price</span>
-                  <button
-                    className={`${styles.btn} ${styles.btnPrimary}`}
-                    onClick={() => {
-                      MPbuyNFT(a.name, a.policyId);
-                    }}
-                  >
-                    <span className={styles.btnIcon}>🛍️</span> Buy
-                  </button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className="flex justify-between">
-        {/* Small NFT box 1 */}
-        <div className="nft-card bg-white shadow-lg p-5 rounded-lg">
-          {/* NFT content */}
-        </div>
-
-        {/* Small NFT box 2 */}
-        <div className="nft-card bg-white shadow-lg p-5 rounded-lg">
-          {/* NFT content */}
-        </div>
-
-        {/* Add more small NFT boxes as needed */}
+                <span className={styles.btnIcon}>🛍️</span> Buy
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
