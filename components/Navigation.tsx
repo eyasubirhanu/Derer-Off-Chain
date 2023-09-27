05936948cdd220994b6eaf33cb1f67bdf5d8a31c
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-27 13:49:11 +0300

    edit navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,40 +1,48 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
-import { useMemo } from "react"
-import { twMerge } from "tailwind-merge"
-import { CardanoWalletSelector } from "use-cardano"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+import { CardanoWalletSelector } from "use-cardano";
 
 const className =
-  "h-10 text-white font-bold tracking-widest uppercase rounded mr-2 px-6 flex items-center dark:hover:text-white bg-blue-300 dark:bg-transparent dark:hover:bg-transparent dark:shadow-none shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300 hover:underline underline-offset-4"
+  "h-10 text-white font-bold tracking-widest uppercase rounded mr-2 px-6 flex items-center dark:hover:text-white bg-blue-300 dark:bg-transparent dark:hover:bg-transparent dark:shadow-none shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300 hover:underline underline-offset-4";
 
 const activeClassName =
-  "text-black dark:text-white ark:bg-transparent dark:shadow-none dark:hover:bg-transparent dark:underline"
+  "text-black dark:text-white ark:bg-transparent dark:shadow-none dark:hover:bg-transparent dark:underline";
 
 export const Navigation = () => {
-  const { asPath } = useRouter()
+  const { asPath } = useRouter();
 
-  const isHome = useMemo(() => asPath === "/", [asPath])
-  const isTransact = useMemo(() => asPath === "/transact", [asPath])
-  const isSign = useMemo(() => asPath === "/sign", [asPath])
-  const isMint = useMemo(() => asPath === "/mint", [asPath])
+  const isHome = useMemo(() => asPath === "/", [asPath]);
+  const isTransact = useMemo(() => asPath === "/transact", [asPath]);
+  const isSign = useMemo(() => asPath === "/sign", [asPath]);
+  const isMint = useMemo(() => asPath === "/mint", [asPath]);
 
   return (
     <nav className="flex h-24 items-center justify-between w-full py-2">
       <div className="flex items-center">
         <Link href="/">
-          <button className={twMerge(className, isHome && activeClassName)}>home</button>
+          <button className={twMerge(className, isHome && activeClassName)}>
+            home
+          </button>
         </Link>
 
         <Link href="/transact">
-          <button className={twMerge(className, isTransact && activeClassName)}>Market-Place</button>
+          <button className={twMerge(className, isTransact && activeClassName)}>
+            Market-Place
+          </button>
         </Link>
 
         <Link href="/sign">
-          <button className={twMerge(className, isSign && activeClassName)}>Sign</button>
+          <button className={twMerge(className, isSign && activeClassName)}>
+            List-YOUR-NFT-TO-MP
+          </button>
         </Link>
 
         <Link href="/mint">
-          <button className={twMerge(className, isMint && activeClassName)}>Mint</button>
+          <button className={twMerge(className, isMint && activeClassName)}>
+            Mint
+          </button>
         </Link>
       </div>
 
@@ -42,5 +50,5 @@ export const Navigation = () => {
         <CardanoWalletSelector />
       </div>
     </nav>
-  )
-}
+  );
+};
