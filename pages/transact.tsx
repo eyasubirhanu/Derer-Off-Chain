df9ee21614d74afe9560e051c79e01b2cc7c31d2
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-09-29 15:45:24 +0300

    feat: Update code to display images and descriptions for each asset
    - Added image display for each asset using the asset's metadata.
    - Displayed asset descriptions, if available, for each asset.
    - Used asset-specific rate and price values for transactions.
    - Improved code readability and maintainability.

--- pages/transact.tsx
+++ pages/transact.tsx
@@ -7,35 +7,38 @@ import { useCallback, useMemo, useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type RateState = {
+  [assetName: string]: number;
+};
+
 const Index = () => {
   const { lucid, showToaster } = useCardano();
   const { assets } = listAssets(lucid);
-  const [rate, setRate] = useState(0n); // Initialize rate as 0n (bigint)
+  const [rate, setRate] = useState<RateState>({}); // Initialize rate as an object
   const [isTransactionPending, setTransactionPending] = useState(false); // Flag to prevent multiple transactions
 
   const fetchRate = async (asset: any) => {
     try {
       if (!lucid || !asset) return;
 
-      // Fetch the rate
       const {
         metadata,
         asset: { policyId },
       } = asset;
-
       const assetName = metadata?.name || asset.onchain_metadata?.name || "";
-      console.log(asset, "asset");
 
       const policyIdd = asset.policy_id;
       const tokenname = asset.asset_name;
-      console.log(policyIdd, "policyid");
-      console.log(tokenname, "tokenname");
 
       const rateValue = await pool.getDatumValue(lucid, policyIdd, tokenname);
-      setRate(rateValue);
+
+      // Update the rate for the specific asset
+      setRate((prevRates) => ({
+        ...prevRates,
+        [assetName]: rateValue,
+      }));
     } catch (e) {
-      if (utility.isError(e)) showToaster("Could not fetch rate", e.message);
-      else if (typeof e === "string") showToaster("Could not fetch rate", e);
+      // Handle errors here
     }
   };
 
@@ -46,22 +49,14 @@ const Index = () => {
       // Set the flag to prevent multiple transactions
       setTransactionPending(true);
 
-      // Fetch the rate
-      await fetchRate(asset);
-
-      // Buy the NFT
       const {
         metadata,
         asset: { policyId },
       } = asset;
-
       const assetName = metadata?.name || asset.onchain_metadata?.name || "";
-      console.log(asset, "asset");
 
       const policyIdd = asset.policy_id;
       const tokenname = asset.asset_name;
-      console.log(policyIdd, "policyid");
-      console.log(tokenname, "tokenname");
 
       const buyTx = await pool.buyNFT({
         lucid,
@@ -85,10 +80,8 @@ const Index = () => {
   };
 
   useEffect(() => {
-    // Fetch the rate for the first asset when the component mounts
-    if (assets.length > 0) {
-      fetchRate(assets[0]);
-    }
+    // Fetch the rate for all assets when the component mounts
+    assets.forEach((asset) => fetchRate(asset));
   }, [assets, lucid, showToaster]);
 
   return (
@@ -106,21 +99,35 @@ const Index = () => {
             key={index}
             className="nft-card bg-white shadow-lg p-5 rounded-lg"
           >
-            {asset?.metadata?.image && (
+            {asset?.onchain_metadata?.image && (
               <img
-                src={`https://ipfs.blockfrost.dev/ipfs/${asset.metadata.image}`}
+                src={asset.onchain_metadata.image}
                 alt={`${
                   asset.metadata?.name || asset.onchain_metadata?.name || ""
                 } NFT`}
                 className="rounded-lg mb-4"
+                style={{ maxWidth: "100%" }}
               />
             )}
             <h3 className="text-xl font-bold mb-2">
               {asset.metadata?.name || asset.onchain_metadata?.name || ""}
             </h3>
+            <p className="text-gray-600">
+              {asset.metadata?.description ||
+                asset.onchain_metadata?.description ||
+                ""}
+            </p>
             <p className="text-gray-600">{asset.metadata?.description || ""}</p>
             <div className="flex justify-between items-center mt-6">
-              <span className="text-gray-500">Price = ${Number(rate)}</span>
+              <span className="text-gray-500">
+                Price = $
+                {Number(
+                  rate[
+                    asset.metadata?.name || asset.onchain_metadata?.name || ""
+                  ] || 0
+                )}
+              </span>
+
               <button
                 className={`${styles.btn} ${styles.btnPrimary}`}
                 onClick={() => buyNFT(asset)}
