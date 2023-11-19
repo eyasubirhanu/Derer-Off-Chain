b0e753bc318f4d7abc17ed12c83a6eeb3c76defd
wendecoder <wenonezra258@gmail.com>
2023-11-20 00:02:40 +0300

    Changes made - modified the slider on the homepage - modified the minting page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,156 +8,175 @@ import { FiArrowRight } from "react-icons/fi";
 const inter = Inter({ subsets: ["latin"] });
 // Define the type for the props
 type NFTCardProps = {
+  slide: number;
   title: string;
   description: string;
   price: string;
 };
-const NFTCard: React.FC<NFTCardProps> = ({ title, description, price }) => (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      className="nft-card flex gap-2 bg-white shadow-lg p-5 rounded-lg"
-    >
-      <img
-        src={`https://source.unsplash.com/featured/400x300/?nft`}
-        alt="NFT Image"
-        className="w-1/2 h-80 rounded-lg mb-4"
-      />
-      <div className="w-1/2">
-      <h3 className="text-xl font-bold mb-2 text-black">{title}</h3>
-      <p className="text-gray-600">{description}</p>
-      <div className="flex ml-52 items-center mt-48 gap-10">
-        <span className="text-gray-500 font-bold text-center bg-slate-300 rounded-md p-6">{price}</span>
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="bg-black p-5 rounded-md font-bold text-center text-xl text-white "
-        >
+const NFTCard: React.FC<NFTCardProps> = ({ slide, title, description, price }) => (
+  <>
+    <img
+      src={`https://source.unsplash.com/featured/400x300/?nft`}
+      alt="NFT Image"
+      className="w-96 h-80 rounded-lg mt-8 ml-8"
+    />
+    <div className="w-1/2 flex flex-wrap -ml-36">
+      <h3 className="text-xl w-full ml-72 mt-8 font-bold mb-2 text-black">{title}</h3>
+      <p className="w-full text-gray-600 ml-56 -mt-10">{description}</p>
+      <div className="flex w-full ml-52 items-center gap-4">
+        <button className="text-amber-400 font-bold bg-lime-700 p-6 rounded-md text-center text-xl hover:bg-amber-700 duration-500">
+          Transfer
+        </button>
+        <span className="font-bold text-black text-center bg-slate-300 rounded-md p-6 w-44 hover:w-56 duration-500">
+          {price}
+        </span>
+        <button className="bg-black p-5 rounded-md font-bold text-center text-xl text-white w-56 hover:w-64 duration-500">
           Buy
-        </motion.button>
+        </button>
       </div>
-      </div>
-    </motion.div>
-  );
-  
+    </div>
+  </>
+);
+
 const Home = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
   const nftData = [
-    { title: "NFT Title 1", description: "NFT Description 1", price: "$500" },
-    { title: "NFT Title 2", description: "NFT Description 2", price: "$600" },
-    { title: "NFT Title 3", description: "NFT Description 3", price: "$700" },
-    { title: "NFT Title 4", description: "NFT Description 4", price: "$800" },
-  ];
+    { title: "Digital Dreams", description: "An abstract digital artwork exploring the beauty of dreams and imagination.", price: "500 ADA" },
+    { title: "Crypto Cosmos", description: "A space-themed NFT capturing the essence of the cryptocurrency universe.", price: "600 ADA" },
+    { title: "Pixel Paradise", description: "A pixel art NFT depicting a serene paradise with vibrant colors and retro aesthetics.", price: "700 ADA" },
+    { title: "Tech Tesseract", description: "Futuristic NFT showcasing the convergence of technology and art in a mesmerizing tesseract.", price: "800 ADA" },
+    { title: "Nature's Symphony", description: "A harmonious blend of natural elements in a mesmerizing symphony of colors and forms.", price: "900 ADA" },
+    { title: "Abstract Elevation", description: "Elevate your digital space with this abstract NFT, exploring the concept of elevation.", price: "1000 ADA" },
+    { title: "Neon Nova", description: "Vibrant and futuristic NFT featuring a neon-lit cityscape with a cosmic twist.", price: "1100 ADA" },
+    { title: "Timeless Treasures", description: "Discover timeless treasures in this NFT, where art meets the echoes of history.", price: "1000 ADA" },
+];
+
 
   const nextCardIndex = (currentCardIndex + 1) % nftData.length;
+  // const previousCardIndex = currentCardIndex == 1 ? 8 : (currentCardIndex - 1);
 
   const handleNextCard = () => {
-    setCurrentCardIndex(nextCardIndex);
+    console.log(currentCardIndex);
+    currentCardIndex == 7
+      ? setCurrentCardIndex(0)
+      : setCurrentCardIndex(nextCardIndex);
+  };
+  const handlePreviousCard = () => {
+    console.log(currentCardIndex);
+    currentCardIndex == 0
+      ? setCurrentCardIndex(7)
+      : setCurrentCardIndex(currentCardIndex - 1);
   };
   return (
-    <motion.div
-      initial={{ opacity: 0, x: -100 }} // Animate from left to right
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.8 }}
-      className="flex flex-wrap w-full text-center mt-20 ml-10 text-white dark:text-gray-100"
-    >
-  <motion.div
-  initial={{ opacity: 0, x: -100 }}
-  animate={{ opacity: 1, x: 0 }}
-  exit={{ opacity: 0 }}
-  transition={{ duration: 0.8, delay: 0.2 }}
-  className="w-1/2 ml-5 rounded-lg -mt-10"
-  style={{
-    backgroundImage: `url('/dererlogo.png')`, // Set the background image URL
-    backgroundSize: 'cover', // Adjust this as needed
-    backgroundPosition: 'center', // Adjust this as needed
-    width: '550px', // Set the width to match the image
-    height: '500px', // Set the height to match the image
-    boxShadow: '0 0 10px 5px rgba(0, 0, 0, 0.5)', // Add a black shadow
-  }}
->
-  {/* You can remove the <img> element */}
-</motion.div>
-
-
-    <motion.div
-        className="w-1/2 block ml-10">
-      <motion.h1
-        style={inter.style}
+    <>
+      <motion.div
         initial={{ opacity: 0, x: -100 }} // Animate from left to right
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
-        className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl"
+        transition={{ duration: 0.8 }}
+        className="flex flex-wrap w-full text-center mt-20 text-white dark:text-gray-100"
       >
-        Derer NFT Marketplace
-      </motion.h1>
-      <motion.p
-        style={inter.style}
-        initial={{ opacity: 0, x: 100 }} // Animate from right to left
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
-        className="mb-6 text-lg w-full font-normal text-gray-100 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
-      >
-        Website That Offers Convenient Payment Options For Ethiopian Customers.
-      </motion.p>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className="w-1/2 ml-5 rounded-lg -mt-10"
+          style={{
+            backgroundImage: `url('/dererlogo.png')`, // Set the background image URL
+            backgroundSize: "cover", // Adjust this as needed
+            backgroundPosition: "center", // Adjust this as needed
+            width: "550px", // Set the width to match the image
+            height: "500px", // Set the height to match the image
+            boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.5)", // Add a black shadow
+          }}
+        ></motion.div>
 
-      <div style={inter.style} className="my-8 text-left">
-        The NFT market in Ethiopia is still extremely small due to challenges
-        such as limited access to cryptocurrencies and a lack of awareness about
-        NFTs and blockchain in general. DERER NFT Marketplace aims to address
-        these challenges and drive mainstream adoption of NFTs in Ethiopia.
-        These challenges of limited technological infrastructure and access can
-        be overcome by developing a mobile-friendly platform and providing
-        alternative forms of payment options accessible to the Ethiopian market.
-        By addressing these limitations, DERER can build a pioneering platform
-        for Ethiopia's NFT community and tap into new markets. Derer NFT
-        Marketplace uses{" "}
-        <a
-          className="underline underline-offset-2"
-          href="https://www.endubis.io/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Endubis-Wallet.
-        </a>
-      </div>
+        <motion.div className="w-1/2 block ml-20">
+          <motion.h1
+            style={inter.style}
+            initial={{ opacity: 0, x: -100 }} // Animate from left to right
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl"
+          >
+            Derer NFT Marketplace
+          </motion.h1>
+          <motion.p
+            style={inter.style}
+            initial={{ opacity: 0, x: 100 }} // Animate from right to left
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            className="mb-6 text-lg w-full font-normal text-gray-100 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+          >
+            Website That Offers Convenient Payment Options For Ethiopian
+            Customers.
+          </motion.p>
+
+          <div style={inter.style} className="my-8 text-left">
+            The NFT market in Ethiopia is still extremely small due to
+            challenges such as limited access to cryptocurrencies and a lack of
+            awareness about NFTs and blockchain in general. DERER NFT
+            Marketplace aims to address these challenges and drive mainstream
+            adoption of NFTs in Ethiopia. These challenges of limited
+            technological infrastructure and access can be overcome by
+            developing a mobile-friendly platform and providing alternative
+            forms of payment options accessible to the Ethiopian market. By
+            addressing these limitations, DERER can build a pioneering platform
+            for Ethiopia's NFT community and tap into new markets. Derer NFT
+            Marketplace uses{" "}
+            <a
+              className="underline underline-offset-2"
+              href="https://www.endubis.io/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Endubis-Wallet.
+            </a>
+          </div>
+        </motion.div>
       </motion.div>
-      <motion.h2
-      style={inter.style}
-      initial={{ opacity: 0, x: -100 }} // Animate from right to left
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.8, delay: 0.2 }}
-      className="text-black relative mt-28 ml-72 font-bold text-4xl text-center 
-      ">
-        Popular NFTS
-      </motion.h2>
-      <div className="container mr-20 mb-20 mt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-        <AnimatePresence initial={false} mode="wait">
-          <NFTCard
-            key={currentCardIndex}
-            title={nftData[currentCardIndex].title}
-            description={nftData[currentCardIndex].description}
-            price={nftData[currentCardIndex].price}
-          />
-        </AnimatePresence>
-      </div>
-      <div className="text-center ml-96">
-        <button
-          onClick={handleNextCard}
-          className="font-bold text-center bg-black rounded-md p-4 text-white mb-5 justify-center -mt-36 ml-52"
+      <motion.div>
+        <motion.h2
+          style={inter.style}
+          initial={{ opacity: 0, x: -100 }} // Animate from right to left
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className="mt-28 text-yellow-400 font-bold text-4xl text-center mb-10 
+      "
         >
-          Next Card
-        </button>
-        <FiArrowRight className="font-4xl"/>
-      </div>
-    </motion.div>
-    
+          Popular NFTS
+        </motion.h2>
+        <div className="w-full">
+          <div className="flex justify-center gap-8">
+            <a href={`#slide${currentCardIndex}`} className="btn btn-square h-56 mt-20" onClick={handlePreviousCard}>
+              ❮
+            </a>
+            <div
+              className="carousel w-3/4 rounded h-96 mb-10"
+            >
+              {nftData.map((data, index) => (
+                <div id={`slide${index}`} className="carousel-item relative w-full bg-slate-400">
+                  <NFTCard
+                    slide={index}
+                    title={data.title}
+                    description={data.description}
+                    price={data.price}
+                  />
+                </div>
+              ))}
+            </div>
+            <a href={`#slide${currentCardIndex}`} className="btn btn-square h-56 mt-20" onClick={handleNextCard}>
+              ❯
+            </a>
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 };
 
--- pages/mint.tsx
+++ pages/mint.tsx
@@ -3,8 +3,10 @@ import * as pool from "lib/poolnfts";
 // import { useAssets } from "hooks/use-assets"
 import { useCallback, useMemo, useState, useEffect } from "react";
 import { useCardano, utility } from "use-cardano";
+import { FaGem } from 'react-icons/fa';
 
 import { Inter } from "@next/font/google";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,70 +51,86 @@ export default function Mint() {
     <div className="text-center text-gray-900 dark:text-gray-100 ">
       <h1
         style={inter.style}
-        className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl py-2"
+        className="mb-4 text-4xl  text-white font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl py-2"
       >
         Derer Nft MarketPlace
       </h1>
+      <p className="text-xl text-yellow-100 font-bold">
+        Mint your very own NFT right here without any hassle!!!
+      </p>
+      <div className="flex mt-15 ml-20 pb-10">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className="w-1/2 ml-5 rounded-lg mt-10"
+          style={{
+            backgroundImage: `url('/MintNft.jpg')`, // Set the background image URL
+            backgroundSize: "cover", // Adjust this as needed
+            backgroundPosition: "center", // Adjust this as needed
+            width: "700px", // Set the width to match the image
+            height: "550px", // Set the height to match the image
+            boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.5)", // Add a black shadow
+          }}
+        ></motion.div>
+        <div className="flex flex-row px-2.5 md:flex-row w-full gap-4 place-content-center justify-center ">
+          <div className="w-[700px] max-w-[90%] h-[550px] mt-10 shadow-sm bg-slate-800 rounded-xl flex items-center flex-col p-8 ">
+            <div className="flex gap-5 rounded-xl text-xl font-bold mb-6 text-white">
+              MINT AN NFT <FaGem className="mt-1.5"/>
+            </div>
 
-      <div className="flex flex-row px-2.5 md:flex-row  justify-center w-full justify-around gap-4 place-content-center justify-center ">
-        <div className="w-[500px] max-w-[90%] h-[550px] mt-10 shadow-sm bg-slate-800 rounded-xl flex items-center flex-col p-8 ">
-          <div className="rounded-xl text-xl font-bold mb-6 text-white">
-            Mint Nft
+            <div className="flex flex-wrap text-left my-8">
+              <label className="w-full">
+                <span className="text-md mb-2 text-white font-bold">
+                  Name for your NFT <span className="text-red-600">*</span>
+                </span>
+              </label>
+              <input
+                className="w-full h-14 rounded py-1 px-2 text-gray-800 border mb-8"
+                name="message"
+                placeholder="Name your NFT"
+                value={name || ""}
+                onChange={(e) => setName(e.target.value?.toString())}
+              />
+
+              <label className="w-full">
+                <span className="text-md mb-2 text-white font-bold">
+                  Your image Location<span className="text-red-600">*</span>
+                </span>
+              </label>
+              <input
+                className="w-full h-14 rounded py-1 px-2 text-gray-800 border mb-8"
+                name="image"
+                placeholder="Enter NFT Image URL or Hash"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+
+              <label className="w-full">
+                <span className="text-md mb-2 text-white font-bold">
+                  Description your NFT<span className="text-red-600">*</span>
+                </span>
+              </label>
+              <textarea
+                className="w-full h-18 rounded py-1 px-2 text-gray-800 border"
+                name="description"
+                placeholder="Enter NFT Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <button
+              disabled={!canMint}
+              className="flex gap-10 justify-center border hover:bg-blue-800 text-white w-64 p-5 cursor-pointer transition-colorsdisabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase mb-10"
+              onClick={() => {
+                hideToaster();
+                mintNFT();
+              }}
+            >
+              Mint <FaGem className="mt-1.5"/> 
+            </button>
           </div>
-
-          <div className="text-left my-8">
-            <div className="my-4">
-              <label className="flex flex-col w-100">
-                <span className="text-sm mb-1 text-white">NFT Name</span>
-
-                <input
-                  className="rounded py-1 px-2 text-gray-800 border"
-                  name="message"
-                  placeholder="My NFT name"
-                  value={name || ""}
-                  onChange={(e) => setName(e.target.value?.toString())}
-                />
-              </label>
-            </div>
-
-            <div className="my-4">
-              <label className="flex flex-col w-100">
-                <span className="text-sm mb-1 text-white">Image URL</span>
-
-                <input
-                  className="rounded py-1 px-2 text-gray-800 border"
-                  name="image"
-                  placeholder="Enter NFT Image URL or Hash"
-                  value={imageUrl}
-                  onChange={(e) => setImageUrl(e.target.value)}
-                />
-              </label>
-            </div>
-
-            <div className="my-4">
-              <label className="flex flex-col w-100">
-                <span className="text-sm mb-1 text-white">Description</span>
-
-                <textarea
-                  className="rounded py-1 px-2 text-gray-800 border"
-                  name="description"
-                  placeholder="Enter NFT Description"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-              </label>
-            </div>
-          </div>
-          <button
-            disabled={!canMint}
-            className="border hover:bg-blue-800 text-white my-4 w-64 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
-            onClick={() => {
-              hideToaster();
-              mintNFT();
-            }}
-          >
-            mint
-          </button>
         </div>
       </div>
     </div>
